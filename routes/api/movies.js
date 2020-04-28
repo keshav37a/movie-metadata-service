cd612f71635c7b92a6123002978eeb9b61f7fd77
Keshav Mathur <keshav37a@gmail.com>
2020-04-28 12:42:17 +0530

    add route for get movie by id

--- routes/api/movies.js
+++ routes/api/movies.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const moviesController = require('../../controllers/api/movies_controller_api');
 
 router.get('/', moviesController.home);
+router.get('/:id', moviesController.getMergedMovieObject);
 
 module.exports = router;
