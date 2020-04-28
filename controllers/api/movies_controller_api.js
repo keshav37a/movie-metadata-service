41c260a93b0bf5ffb6b6e7224b7d7d3f3397f055
Keshav Mathur <keshav37a@gmail.com>
2020-04-28 12:00:50 +0530

    add function to read files from directory

--- controllers/api/movies_controller_api.js
+++ controllers/api/movies_controller_api.js
@@ -1,6 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+
 module.exports.home = function(req, res){
     console.log('home in movies_controller_api called');
+
+    readFromFiles();
+
     return res.status(200).json({
         message: 'home in movies_controller_api called'
     });
+}
+
+let readFromFiles = async function(){
+    
+    try{
+        let finalJSONArray = [];
+        let moviesPath = path.join(__dirname, '../../movies');
+        let fileNames = await fs.readdirSync(moviesPath);
+        fileNames.forEach((element)=>{
+            let singleMovieFilePath = path.join(moviesPath, '/', element);
+            let rawData = fs.readFileSync(singleMovieFilePath);
+            let data = JSON.parse(rawData);
+            finalJSONArray.push(data);
+        })
+        console.log(finalJSONArray);
+    }
+    catch(err){
+        console.log(`${err}`);
+    }
+    
 }
