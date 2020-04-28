52766295e8d218d996c9ff47c706548e2f50519f
Keshav Mathur <keshav37a@gmail.com>
2020-04-28 12:49:57 +0530

    add retrieve movie details from omdb api

--- controllers/api/movies_controller_api.js
+++ controllers/api/movies_controller_api.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 const path = require('path');
+const keys = require('../../keys');
+const axios = require('axios').default;
+
 
 module.exports.home = function(req, res){
     console.log('home in movies_controller_api called');
@@ -9,6 +12,26 @@ module.exports.home = function(req, res){
     return res.status(200).json({
         message: 'home in movies_controller_api called'
     });
+}
+
+module.exports.getMergedMovieObject = async function(req, res){
+    let paramId = req.params.id;
+    readMovieFromServer(paramId);
+    console.log(paramId);
+    let obj = await readMovieFromServer(paramId);
+    // console.log(obj.response);
+    if(obj.isSuccessful){
+        return res.status(200).json({
+            data: obj.response,
+            message: `getMergedMovieObject called for id ${paramId}`
+        });
+    }
+    else{
+        return res.status(200).json({
+            message: obj.response
+        });
+    }
+    
 }
 
 let readFromFiles = async function(){
@@ -23,10 +46,37 @@ let readFromFiles = async function(){
             let data = JSON.parse(rawData);
             finalJSONArray.push(data);
         })
-        console.log(finalJSONArray);
+        // console.log(finalJSONArray);
     }
     catch(err){
         console.log(`${err}`);
     }
     
+}
+
+let readMovieFromServer = async function(imdbId){
+    let api_key = keys;
+    console.log(imdbId);
+    let url = `http://www.omdbapi.com/?i=${imdbId}&apikey=${api_key}&plot=full`;
+    console.log(url);
+    let isSuccessful = false;
+    let obj = {};
+    try{
+        let response = await axios.get(url);
+        
+        if(response){
+            isSuccessful = true;
+            obj.response = response.data;
+            // console.log(obj.response);
+            obj.isSuccessful = isSuccessful;
+            return obj;
+        }
+    }
+    catch(err){
+        console.log('error');
+        isSuccessful = false;
+        obj.response = err;
+        obj.isSuccessful = isSuccessful;
+        return obj;
+    }
 }
